483b1d5e1ac0467cb353402ed015b88561021071
Ayush Garg <ayush181000@gmail.com>
2021-12-07 01:21:48 +0530

    Generics Challenge

---
+++ Generics Challenge/src/com/ayush/Main.java
@@ -0,0 +1,30 @@
+package com.ayush;
+
+public class Main {
+
+    public static void main(String[] args) {
+        League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
+
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+
+
+        footballLeague.add(melbourne);
+        footballLeague.add(adelaideCrows);
+        footballLeague.add(hawthorn);
+        footballLeague.add(fremantle);
+
+//        footballLeague.add(baseballTeam);
+
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(adelaideCrows, 3, 8);
+        adelaideCrows.matchResult(fremantle, 2, 1);
+
+        footballLeague.showLeagueTable();
+    }
+}
---
+++ Generics Challenge/src/com/ayush/Player.java
@@ -0,0 +1,15 @@
+package com.ayush;
+
+public abstract class Player {
+    private final String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
