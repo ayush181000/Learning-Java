33ae8705e7f016a6bad032ed5e7a30f7868e02de
Ayush Garg <ayush181000@gmail.com>
2021-12-24 03:45:59 +0530

    Immutable class challenge

--- Immutable Class Challenge/src/com/ayush/Location.java
+++ Immutable Class Challenge/src/com/ayush/Location.java
@@ -12,8 +12,11 @@ public class Location {
     public Location(int locationId, String description, Map<String, Integer> exits) {
         this.locationId = locationId;
         this.description = description;
-        this.exits = new HashMap<String, Integer>(exits);
-
+        if (exits != null) {
+            this.exits = new HashMap<>(exits);
+        } else {
+            this.exits = new HashMap<>();
+        }
         this.exits.put("Q", 0);
     }
 
